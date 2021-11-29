42914f4585948f6e86fe3bb9dc0f684f2fd959e6
Purgen42 <askusakin@gmail.com>
2021-11-30 00:01:20 +0400

    Remove ParseException

--- src/main/java/ru/javawebinar/topjava/formatter/LocalTimeFormatter.java
+++ src/main/java/ru/javawebinar/topjava/formatter/LocalTimeFormatter.java
@@ -3,13 +3,12 @@ package ru.javawebinar.topjava.formatter;
 import org.springframework.format.Formatter;
 import ru.javawebinar.topjava.util.DateTimeUtil;
 
-import java.text.ParseException;
 import java.time.LocalTime;
 import java.util.Locale;
 
 public class LocalTimeFormatter implements Formatter<LocalTime> {
     @Override
-    public LocalTime parse(String text, Locale locale) throws ParseException {
+    public LocalTime parse(String text, Locale locale) {
         return DateTimeUtil.parseLocalTime(text);
     }
 
